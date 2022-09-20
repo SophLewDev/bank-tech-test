bc10a547e983649dcdfa757a9fbe07b515d6d740
sophie <soph_lew@hotmail.co.uk>
2022-09-20 19:54:44 +0100

    tests for returning amount of withdrawal, passing

--- unit/withdrawal.test.js
+++ unit/withdrawal.test.js
@@ -5,4 +5,8 @@ describe("deposit", () => {
     const withdrawal = new Withdrawal("20/09/2022",1000)
     expect(withdrawal.date).toEqual("20/09/2022")
   });
+  it("returns amount of withdrawal", () => {
+    const withdrawal = new Withdrawal("20/09/2022",1000)
+    expect(withdrawal.amount).toEqual(1000)
+  });
 });
