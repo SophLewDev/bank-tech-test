7ea404d6bd3760cb92601d6fcc01c987f1b233e9
sophie <soph_lew@hotmail.co.uk>
2022-09-20 20:04:32 +0100

    test for adding withdrawal to account, passing

--- integration/account.test.js
+++ integration/account.test.js
@@ -1,5 +1,6 @@
 const Account = require('../account')
 const Deposit = require('../deposit')
+const Withdrawal = require('../withdrawal')
 
 describe("account", () => {
   it("add deposit to account and return deposit", () => {
@@ -7,5 +8,11 @@ describe("account", () => {
     const account = new Account()
     account.addToAccount(deposit)
     expect(account.account).toEqual([deposit])
+  })
+  it("add withdrawal to account and return withdrawal", () => {
+    const withdrawal = new Withdrawal("20/09/2022",1000)
+    const account = new Account()
+    account.addToAccount(withdrawal)
+    expect(account.account).toEqual([withdrawal])
   })
 })
